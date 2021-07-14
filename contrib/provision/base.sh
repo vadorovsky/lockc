93e6e2304d9506510d5e4d422a1c6127e117718e
Michal Rostecki <mrostecki@opensuse.org>
2021-07-14 18:45:24 +0200

    provision: Do not copy config files
    After this change, uploading config files to the VM images will be
    handled by Packer, without mounting the full git repo to the VM yet.
    
    Signed-off-by: Michal Rostecki <mrostecki@opensuse.org>

--- contrib/provision/base.sh
+++ contrib/provision/base.sh
@@ -23,11 +23,6 @@ zypper install -y \
        tmux \
        wget
 
-pushd /home/vagrant/enclave
-install -D -m 0644 contrib/etc/modules-load.d/99-k8s.conf /etc/modules-load.d/99-k8s.conf
-install -D -m 0644 contrib/etc/sysctl.d/99-k8s.conf /etc/sysctl.d/99-k8s.conf
-popd
-
 sed -i -e "s/GRUB_CMDLINE_LINUX=.*/GRUB_CMDLINE_LINUX=\"lsm=bpf,integrity\"/" \
     /etc/default/grub
 grub2-mkconfig -o /boot/grub2/grub.cfg
--- contrib/provision/build.sh
+++ contrib/provision/build.sh
@@ -5,6 +5,4 @@ cd /home/vagrant/enclave
 export CLANG=/usr/bin/clang-12
 cargo install --path .
 
-sudo install -D -m 0644 contrib/systemd/enclave.service /etc/systemd/system/enclave.service
-sudo systemctl enable enclave.service
-sudo systemctl start enclave.service
+sudo systemctl enable --now enclave.service
